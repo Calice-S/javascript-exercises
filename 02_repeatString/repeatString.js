379abf4a037d69e2204084467edf8b0481a8e944
Calice <calice96@gmail.com>
2023-01-08 00:18:31 -0500

    first exercise down

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let strWord = '';
+    if(num < 0) {
+        return 'ERROR';
+    }
+    for(let i = 1; i <= num; i++) {
+       strWord += word;
+    }
+    return strWord;
 };
 
 // Do not edit below this line
