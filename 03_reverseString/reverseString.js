689cacb15ec0ff7559ad7bea9b9b63fe328fd20b
Calice <calice96@gmail.com>
2023-01-20 22:14:27 -0500

    reverse a string done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let text = str;
+    const arr = text.split("");
+   
+    let reverseText = arr.reverse();
+    
+    let joinText = reverseText.join("");
+    
+    return joinText;
 };
 
 // Do not edit below this line
