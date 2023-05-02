247cbb37e68bdd32c4ede92725ec82a8c38654ca
Calice <calice96@gmail.com>
2023-05-01 22:15:27 -0400

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+let sum = 0;
+let num = 0; 
+
+if(typeof(min) !== "number" || typeof(max) !== "number") {
+    return "ERROR";
+}
+
+if(min < max) {
+    num = max;
+} else {
+    num = min;
+}
+
+if(min > 0 && max > 0){
+    for(let i = 1; i <= num; i++) {
+        sum += i;
+    }
+} else {
+    return "ERROR";
+}
+
+
+
+return sum;
 
 };
 
